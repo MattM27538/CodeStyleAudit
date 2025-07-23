546cf57f63b35757cf6b30e83fa53389369a27c9
Matthew Martinez <mmartinez18@mail.csuchico.edu>
2025-07-22 23:32:25 -0700

    Handle opening code file to style check

--- codeStyleChecker.c
+++ codeStyleChecker.c
@@ -2,12 +2,22 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include "library.h"
+#include <assert.h>
 
 
 int main(int argc, char* argv[]){
     if(!correctCMDLineInput(argc)){
-        exit(1);
+        return EXIT_FAILURE;
     }
-
+    
+    FILE* codeFile;
+    codeFile=fopen(argv[1], "r");
+    
+    if(codeFile == NULL){
+        puts("Error opening code file. Please try again.");
+        return EXIT_FAILURE;
+    }
+    
+    fclose(codeFile);
     return 0;
 }
