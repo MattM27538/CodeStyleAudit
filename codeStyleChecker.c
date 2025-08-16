bcc19bb5e4779cc5b5b61fab5880d15274731a5d
Matthew Martinez <mmartinez18@mail.csuchico.edu>
2025-08-16 00:17:19 -0700

    Add read lines from code file functionality

--- codeStyleChecker.c
+++ codeStyleChecker.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include "library.h"
 #include <assert.h>
+#include <stdint.h>
 
 
 int main(int argc, char* argv[]){
@@ -11,11 +12,36 @@ int main(int argc, char* argv[]){
     }
     
     FILE* codeFile;
-    codeFile=fopen(argv[1], "r");
+    const char* codeFileName=argv[1];
+    const char* readOnly="r";
+
+    codeFile=fopen(codeFileName, readOnly);
     
     if(codeFile == NULL){
-        puts("Error opening code file. Please try again.");
+        perror("Error opening code file. Please try again.");
         return EXIT_FAILURE;
+    }
+
+    char charInCode;
+    //Consider putting these in a struct.
+    const uint8_t maxLineSize=100;
+    uint8_t currentLineSize=0;
+    long long lineNumber=1;
+
+    while((charInCode=fgetc(codeFile)) != EOF){
+            if(charInCode == '\n'){
+                currentLineSize=0;
+                ++lineNumber;
+            }
+
+            ++currentLineSize;
+
+            if(currentLineSize > maxLineSize){
+                printf("Line %lld exceeds limit of 100 characters. Correct your code and recompile.", lineNumber);
+                return EXIT_FAILURE;
+            }
+
+            // fputc(charInCode, stdout);
     }
     
     fclose(codeFile);
