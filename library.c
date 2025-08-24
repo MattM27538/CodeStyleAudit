5f5db357b31d86df0adbaec1e4b67c0504eff4e2
Matthew Martinez <mmartinez18@mail.csuchico.edu>
2025-08-24 00:46:57 -0700

    Add isWhileLoop function

--- library.c
+++ library.c
@@ -1,5 +1,6 @@
 #include <stdbool.h>
 #include "library.h"
+#include <string.h>
 
 //DELETE ME when done testing.
 #include <stdio.h>
@@ -15,7 +16,7 @@ bool correctCMDLineInput(int argc){
     return false;
 }
 
-bool isComment(char* lineOfCode, int lineSize, bool* isMultiLineComment){
+bool isComment(char* lineOfCode, const int lineSize, bool* isMultiLineComment){
     // printf("Line is of size %d\n", lineSize);
 
     for(int i=0; i < lineSize; ++i){
@@ -36,6 +37,36 @@ bool isComment(char* lineOfCode, int lineSize, bool* isMultiLineComment){
             *isMultiLineComment=false;
             return true;
         }
+    }
+
+    return false;
+}
+
+bool isWhileLoop(const char* lineOfCode, const int lineSize){
+    const char* whileLiteral="while";
+
+    char placeHolderName[6];
+
+    for(int i=0; i < lineSize; ++i){
+        if(lineOfCode[i] == ' '){
+                continue;
+        }
+
+        for(size_t j=0; j<strlen(whileLiteral); ++j){
+            if(lineOfCode[i] == '\n'){
+                puts("returned false\n");
+                return false;
+            }
+            
+            // printf("%zu ", i+j);
+            placeHolderName[j]=lineOfCode[i+j];
+        }
+
+        placeHolderName[5]='\0';
+        // printf("%d\n", strncmp(whileLiteral, placeHolderName, strlen(whileLiteral)));
+        // printf("%s =? ", placeHolderName);
+        // printf("%s\n", whileLiteral);
+        return !(strncmp(whileLiteral, placeHolderName, strlen(whileLiteral)));
     }
 
     return false;
