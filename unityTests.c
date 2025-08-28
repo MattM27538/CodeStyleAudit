ba5f0a5da265838e751e5ce777286d36eacb2aee
Matthew Martinez <mmartinez18@mail.csuchico.edu>
2025-08-27 18:15:46 -0700

    Change trueBool variable name

--- unityTests.c
+++ unityTests.c
@@ -8,22 +8,29 @@ void setUp(){
 
 }
 
-void test_correctCMDLineInput(){
+void testCorrectCMDLineInput(){
     TEST_ASSERT(correctCMDLineInput(2) == true);
     TEST_ASSERT(correctCMDLineInput(1) == false);
     TEST_ASSERT(correctCMDLineInput(INT_MAX) == false);
 }
 
-void test_checkForComments(){
-    bool trueBool=true;
+void testCheckForComments(){
+    bool isMultiLineComment=true;
 
-    TEST_ASSERT(isComment("  //", 4, &trueBool) == true);
-    TEST_ASSERT(isComment("  /*", 4, &trueBool) == true);
-    TEST_ASSERT(isComment("  //*", 5, &trueBool) == true);
-    TEST_ASSERT(isComment("char myString[5]=\"//*\"", 16, &trueBool) == false);
-    TEST_ASSERT(isComment("for()", 5, &trueBool) == false);
-    TEST_ASSERT(isComment("  while()", 9, &trueBool) == false);
-    TEST_ASSERT(isComment("", 0, &trueBool) == false);
+    TEST_ASSERT(isComment("  //", 4, &isMultiLineComment) == true);
+    TEST_ASSERT(isComment("  /*", 4, &isMultiLineComment) == true);
+    TEST_ASSERT(isComment("  //*", 5, &isMultiLineComment) == true);
+    TEST_ASSERT(isComment("char myString[5]=\"//*\"", 16, &isMultiLineComment) == false);
+    TEST_ASSERT(isComment("for()", 5, &isMultiLineComment) == false);
+    TEST_ASSERT(isComment("  while()", 9, &isMultiLineComment) == false);
+    TEST_ASSERT(isComment("", 0, &isMultiLineComment) == false);
+}
+
+void testIsWhileLoop(){
+    TEST_ASSERT(isWhileLoop("", 0) == false);
+    TEST_ASSERT(isWhileLoop("  while(1)", 10) == true);
+    TEST_ASSERT(isWhileLoop("//while(1)", 10) == false);
+    TEST_ASSERT(isWhileLoop("/*while(1)*/", 12) == false);
 }
 
 
@@ -34,9 +41,11 @@ void tearDown(){
 int main(){
     UNITY_BEGIN();
 
-    RUN_TEST(test_correctCMDLineInput);
+    RUN_TEST(testCorrectCMDLineInput);
 
-    RUN_TEST(test_checkForComments);
+    RUN_TEST(testCheckForComments);
+
+    RUN_TEST(testIsWhileLoop);
 
     return UNITY_END();
 }
